18afd05a2aa4fd236accdd851bce58554c26285a
Sanket Tavarageri <sanketst@gmail.com>
2017-08-31 23:19:05 -0700

    Edits

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,7 +2,7 @@
 
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SimpleApp {
 
@@ -13,10 +13,29 @@ object SimpleApp {
   }
 
   def csvToDf(spark: SparkSession) = {
-    val df = spark.read
+    val orig_df = spark.read
       .option("header", "true") //reading the headers
       .csv(getClass.getClassLoader.getResource("medicare_payment_2011.csv").getPath)
 
-    df.take(10).foreach(row => println("ROW: " + row))
+    val df = normalizeHeaders(orig_df)
+    calculateStats(df)
+//    df.take(10).foreach(row => println("ROW: " + row))
+  }
+
+  def normalizeHeaders(df: DataFrame) = {
+    var newDf = df
+    for(col <- df.columns){
+      newDf = newDf.withColumnRenamed(col,col.replaceAll("\\s", "_"))
+    }
+
+    newDf
+  }
+
+  def calculateStats(df: DataFrame): Unit = {
+    df.createOrReplaceTempView("payment")
+    df.sparkSession.sql("SELECT Provider_Zip_Code, COUNT(*) as count " +
+      "FROM payment GROUP BY Provider_Zip_Code").show()
+
+    df.sparkSession.sql("SELECT DISTINCT DRG_Definition FROM payment").show(10000)
   }
 }
